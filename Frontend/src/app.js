92e2abf85f53c45c5ad6ba246cf874dcfa756880
Faysal Mahmud <faysalmahmud735@gmail.com>
2023-10-23 02:12:05 -0700

    app.js

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -1,22 +1,3 @@
-// import { useState } from "react";
-
-// import AuthPage from "./authPage";
-// import ChatsPage from "./chatsPage";
-
-// function App() {
-//   const [user, setUser] = useState();
-
-//   if (!user) {
-//     return <AuthPage onAuth={(user) => setUser(user)} />;
-//   } else {
-//     return <ChatsPage user={user} />;
-//   }
-// }
-
-// export default App;
-
-
-// reload implementation
 import { useState, useEffect } from "react";
 
 import AuthPage from "./authPage";
